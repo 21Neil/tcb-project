db22dbfa8964fe35eee64257415b4f4d27fc6158
Neil <88711171+21Neil@users.noreply.github.com>
2022-11-17 16:21:10 +0800

    Updates

---
+++ precache-manifest.ea0ef95c618fe2981e9402cfb4a1df38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "330c4969b4c453555fcf",
+    "url": "/tcb-project/static/css/main.aa05e831.chunk.css"
+  },
+  {
+    "revision": "330c4969b4c453555fcf",
+    "url": "/tcb-project/static/js/main.330c4969.chunk.js"
+  },
+  {
+    "revision": "fbee9149cc341e4ff9a0",
+    "url": "/tcb-project/static/js/1.fbee9149.chunk.js"
+  },
+  {
+    "revision": "d1c5c79719a77da74f48",
+    "url": "/tcb-project/static/js/2.d1c5c797.chunk.js"
+  },
+  {
+    "revision": "98927a77fc4ce3f49880",
+    "url": "/tcb-project/static/js/runtime~main.98927a77.js"
+  },
+  {
+    "revision": "fb35898ddc55f1ca62f4443d35091753",
+    "url": "/tcb-project/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.166069451a00ef86ef5cb3f76c69108e.js"
+  "/tcb-project/precache-manifest.ea0ef95c618fe2981e9402cfb4a1df38.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/tcb-project/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
